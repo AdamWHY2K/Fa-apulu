181f38c56dd59cb1d563a1c7b13743510bea68d3
AdamWHY2K <68286215+AdamWHY2K@users.noreply.github.com>
2021-07-29 22:50:09 +0100

    Update bat_getName.py
    Added comments

--- @Resources/scripts/bat_getName.py
+++ @Resources/scripts/bat_getName.py
@@ -7,8 +7,6 @@ readData = PyVDF()
 completeDict = {}
 foundAccount = None
 
-print(getcwd())
-
 path = input('Paste steam installation directory, e.g "C:\\Program Files\\Steam"\n\n')
 
 accountDir = listdir(path + "\\userdata")
@@ -25,19 +23,24 @@ for i in accountDir:
     else:
         #print("skipping non digits ", accountDir[count], "\nPlace in list: ", count)
         count += 1
+    #Steam has two folders in the user directory that don't belong to users, "0" and "ac" so we skip both of them.
 
 count = 0
 line = []
 
 for i in curatedDir:
+    #Iterating through all user folders in user directory.
     try:
         readData.load(path + "\\userdata\\" + curatedDir[count] + "\\config\\localconfig.vdf")
         readData = readData.getData()
         readData["UserLocalConfigStore"]["friends"]["PersonaName"]
         completeDict.update({readData["UserLocalConfigStore"]["friends"]["PersonaName"]:curatedDir[count]})
+        #Adding user's steam name and steam account identifier to a dictionary.
         readData = PyVDF()
         count += 1
     except UnicodeDecodeError:
+        #If user has a steam friend or steam group that has non standard characters PyVDF will throw a UnicodeDecodeError exception.
+        #In this case we simply use open() with errors ignored, its uglier but functional.
         with open(path + "\\userdata\\" + curatedDir[count] + "\\config\\localconfig.vdf", "r", errors='ignore') as altRead:
             line = altRead.readline(altRead.seek(altRead.read().find("\"PersonaName\"")))
             completeDict.update({line[16:-2]:curatedDir[count]})

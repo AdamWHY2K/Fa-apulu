e6cf1260ca440146d9103f95618eb2ed8c3c247b
AdamWHY2K <68286215+AdamWHY2K@users.noreply.github.com>
2021-08-12 16:48:52 +0100

    Update bat_getName.py
    Some users have a valid steam id folder, yet no localconfig.vdf, I didn't know this was possible. Added a check to see if localconfig.vdf exists in valid folders.

--- @Resources/scripts/bat_getName.py
+++ @Resources/scripts/bat_getName.py
@@ -1,4 +1,4 @@
-from os import listdir, getcwd
+from os import listdir, getcwd, path
 from PyVDF import PyVDF
 
 count = 0
@@ -7,19 +7,19 @@ readData = PyVDF()
 completeDict = {}
 foundAccount = None
 
-path = input('Paste steam installation directory, e.g "C:\\Program Files\\Steam"\n\n')
+steamPath = input('Paste steam installation directory, e.g "C:\\Program Files\\Steam"\n\n')
 
 while True:
-    if "Steam" not in path or ":" not in path or "\\" not in path:
+    if "Steam" not in steamPath or ":" not in steamPath or "\\" not in steamPath:
         print("Incorrect path detected, please try again\n\n")
-        path = input("Paste steam installation directory (The folder that steam.exe resides)\n\n")
+        steamPath = input("Paste steam installation directory (The folder that steam.exe resides)\n\n")
     else:
         break
     #While loop keeps user from entering a tragically incorrect path
 try:
-    accountDir = listdir(path + "\\userdata")
+    accountDir = listdir(steamPath + "\\userdata")
 except FileNotFoundError:
-    print("Critical error, check your steam directory. " + path + "\\userdata doesn't exist?\n")
+    print("Critical error, check your steam directory. " + steamPath + "\\userdata doesn't exist?\n")
     input("Press enter then retry.\n")
     exit()
     #Try block catches exception if user enters the wrong steam path.
@@ -43,22 +43,25 @@ line = []
 
 for i in curatedDir:
     #Iterating through all user folders in user directory.
-    try:
-        readData.load(path + "\\userdata\\" + curatedDir[count] + "\\config\\localconfig.vdf")
-        readData = readData.getData()
-        readData["UserLocalConfigStore"]["friends"]["PersonaName"]
-        completeDict.update({readData["UserLocalConfigStore"]["friends"]["PersonaName"]:curatedDir[count]})
-        #Adding user's steam name and steam account identifier to a dictionary.
-        readData = PyVDF()
-        count += 1
-    except UnicodeDecodeError:
-        #If user has a steam friend or steam group that has non standard characters PyVDF will throw a UnicodeDecodeError exception.
-        #In this case we simply use open() with errors ignored, its uglier but functional.
-        with open(path + "\\userdata\\" + curatedDir[count] + "\\config\\localconfig.vdf", "r", errors='ignore') as altRead:
-            line = altRead.readline(altRead.seek(altRead.read().find("\"PersonaName\"")))
-            completeDict.update({line[16:-2]:curatedDir[count]})
-            line = []
+    if path.isfile(steamPath + "\\userdata\\" + curatedDir[count] + "\\config\\localconfig.vdf"):
+        try:
+            readData.load(steamPath + "\\userdata\\" + curatedDir[count] + "\\config\\localconfig.vdf")
+            readData = readData.getData()
+            readData["UserLocalConfigStore"]["friends"]["PersonaName"]
+            completeDict.update({readData["UserLocalConfigStore"]["friends"]["PersonaName"]:curatedDir[count]})
+            #Adding user's steam name and steam account identifier to a dictionary.
+            readData = PyVDF()
             count += 1
+        except UnicodeDecodeError:
+            #If user has a steam friend or steam group that has non standard characters PyVDF will throw a UnicodeDecodeError exception.
+            #In this case we simply use open() with errors ignored, its uglier but functional.
+            with open(steamPath + "\\userdata\\" + curatedDir[count] + "\\config\\localconfig.vdf", "r", errors='ignore') as altRead:
+                line = altRead.readline(altRead.seek(altRead.read().find("\"PersonaName\"")))
+                completeDict.update({line[16:-2]:curatedDir[count]})
+                line = []
+                count += 1
+    else:
+        pass
 
 found = False
 
@@ -71,4 +74,4 @@ for i in completeDict:
             break
 
 with open("@Resources\\LocalVariables.txt", "w") as outFile:
-    outFile.write(path + "\n" + foundAccount)
+    outFile.write(steamPath + "\n" + foundAccount)
